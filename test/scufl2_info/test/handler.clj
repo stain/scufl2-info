efbe424f1df6ead7509cf2586a33136b228bd964
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2014-03-27 22:35:22 +0000

    test processor

--- test/scufl2_info/test/handler.clj
+++ test/scufl2_info/test/handler.clj
@@ -5,6 +5,7 @@
         scufl2-info.handler))
 
 (defn test-workflow-bundle [wfbundle]
+    (is (not (nil? wfbundle)))
     (is (= (get wfbundle "@id") "http://ns.taverna.org.uk/2010/workflowBundle/62eb2413-bfec-4947-9854-cbabc7ecbc32/" ))
     (is (= (get wfbundle "@type") "WorkflowBundle"))
     (is (contains? wfbundle "@context"))
@@ -12,9 +13,16 @@
     (is (= (get-in wfbundle ["@context" "@base"]) "http://ns.taverna.org.uk/2010/workflowBundle/62eb2413-bfec-4947-9854-cbabc7ecbc32/")))
 
 (defn test-workflow [workflow]
+  (is (not (nil? workflow)))
   (is (= (get workflow "name") "HelloWorld"))
   (is (= (get workflow "@id") "workflow/HelloWorld/"))
   (is (= (get workflow "@type") "Workflow")))
+
+(defn test-processor [processor]
+  (is (not (nil? processor)))
+  (is (= (get processor "name") "hello"))
+  (is (= (get processor "@id") "workflow/HelloWorld/processor/hello/"))
+  (is (= (get processor "@type") "Processor")))
 
 (deftest test-app
   (testing "main route"
@@ -27,7 +35,6 @@
       (is (= (:status response) 200))
       (let [json (parse-string (:body response))]
         (test-workflow-bundle json))))
-
   
   (testing "workflow"
     (let [response (app (request :get "/workflowBundle/62eb2413-bfec-4947-9854-cbabc7ecbc32/workflow/HelloWorld/"))]
@@ -35,6 +42,14 @@
       (let [json (parse-string (:body response))]
         (test-workflow-bundle json)
         (test-workflow (get json "workflow")))))
+
+  (testing "processor"
+    (let [response (app (request :get "/workflowBundle/62eb2413-bfec-4947-9854-cbabc7ecbc32/workflow/HelloWorld/processor/hello/"))]
+      (is (= (:status response) 200))
+      (let [json (parse-string (:body response))]
+        (test-workflow-bundle json)
+        (test-workflow (get json "workflow"))
+        (test-processor (get-in json ["workflow" "processor"])))))
 
   
   (testing "not-found route"
