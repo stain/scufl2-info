f0fb778e5884a714a5f1b17f8744524559999d25
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2014-03-27 22:23:41 +0000

    test wfbundle

--- test/scufl2_info/test/handler.clj
+++ test/scufl2_info/test/handler.clj
@@ -1,6 +1,7 @@
 (ns scufl2-info.test.handler
   (:use clojure.test
         ring.mock.request  
+        cheshire.core 
         scufl2-info.handler))
 
 (deftest test-app
@@ -8,6 +9,17 @@
     (let [response (app (request :get "/"))]
       (is (= (:status response) 200))
       (is (= (:body response) "Hello World"))))
+
+  (testing "workflow bundle"
+    (let [response (app (request :get "/workflowBundle/62eb2413-bfec-4947-9854-cbabc7ecbc32/"))]
+      (is (= (:status response) 200))
+      (let [json (parse-string (:body response))]
+        (is (= (get json "@id") "http://ns.taverna.org.uk/2010/workflowBundle/62eb2413-bfec-4947-9854-cbabc7ecbc32/" ))
+        (is (= (get json "@type") "WorkflowBundle"))
+        (is (contains? json "@context"))
+        (is (= (get-in json ["@context" "@vocab"]) "http://ns.taverna.org.uk/2010/scufl2#"))
+        (is (= (get-in json ["@context" "@base"]) "http://ns.taverna.org.uk/2010/workflowBundle/62eb2413-bfec-4947-9854-cbabc7ecbc32/")))))
+
   
   (testing "not-found route"
     (let [response (app (request :get "/invalid"))]
