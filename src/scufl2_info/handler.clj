259ff2047ba85e674260f2c9faef4f0302d39dd8
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2014-03-28 01:44:41 +0000

    explanation/examples on index page HTML

--- src/scufl2_info/handler.clj
+++ src/scufl2_info/handler.clj
@@ -108,7 +108,27 @@
 
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] "
+              <h1>scufl2-info</h1>
+              This is the <a href='https://github.com/stain/scufl2-info'>scufl2-info</a> web service.
+              <p>
+              This service generates <a href='http://json-ld.org/'>JSON-LD</a> Linked Data descriptions for 
+              <a href='http://dev.mygrid.org.uk/wiki/display/developer/SCUFL2'>SCUFL2</a> resources,
+              following the same URI syntax as below the base
+              <code>http://ns.taverna.org.uk/2010/workflowBundle/</code>
+              <p>
+              Examples:
+              <ul>
+                <li> <a href='workflowBundle/2f0e94ef-b5c4-455d-aeab-1e9611f46b8b/'>workflow bundle</a></li>
+                <li> <a href='workflowBundle/2f0e94ef-b5c4-455d-aeab-1e9611f46b8b/workflow/HelloWorld/'>workflow</a></li>
+                <li> <a href='workflowBundle/2f0e94ef-b5c4-455d-aeab-1e9611f46b8b/workflow/HelloWorld/in/input1'>workflow input port</a></li>
+                <li> <a href='workflowBundle/2f0e94ef-b5c4-455d-aeab-1e9611f46b8b/workflow/HelloWorld/processor/hello/'>processor</a></li>
+                <li> <a href='workflowBundle/2f0e94ef-b5c4-455d-aeab-1e9611f46b8b/workflow/HelloWorld/processor/hello/out/output1'>processor output port</a></li>
+                <li> <a href='workflowBundle/2f0e94ef-b5c4-455d-aeab-1e9611f46b8b/workflow/HelloWorld/datalink?from=processor/hello/out/output1&to=out/result'>datalink</a></li>
+              </ul>
+              <p>
+              Questions? Contact support@mygrid.org.uk
+              ")
   ; TODO: Check that uuid is a valid uuid, else 404 on all below
   (GET "/workflowBundle/:uuid/" 
        [uuid] (check-uuid (wfbundle-json uuid)))
