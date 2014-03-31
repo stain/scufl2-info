0ff6005058f35a726d5e23c6074be389fa5580cf
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2014-03-31 18:14:32 +0100

    Explicit dependencies; version mismatch somewhere
    FIXME: Investigate and report upstream

--- project.clj
+++ project.clj
@@ -1,7 +1,14 @@
 (defproject scufl2-info "0.2.0-SNAPSHOT"
   :description "SCUFL2 information service"
   :url "https://github.com/stain/scufl2-info"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [
+                 [org.clojure/clojure "1.5.1"]
+                 ;; FIXME: These dependencies should have been found 
+                 ;; transitively!!
+                 [org.clojure/tools.reader "0.7.3"]
+                 [commons-fileupload/commons-fileupload "1.3"]
+                 ;; Temporary for offline-use - upgrade commons-io
+                 [commons-io/commons-io "2.4"]
                  [ring/ring-json "0.3.0"]
                  [compojure "1.1.6"]]
   :plugins [[lein-ring "0.8.10"]]
