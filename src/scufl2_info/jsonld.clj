311f11df3819a04987a90dc1270a4751369ffde6
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2014-04-20 19:46:20 +0100

    support for ntriples and rdfxml

--- src/scufl2_info/jsonld.clj
+++ src/scufl2_info/jsonld.clj
@@ -15,14 +15,19 @@
 ; Initialize Jena binding once -- is this needed as long as we do import?
 (JenaJSONLD/init)
 
-(defn jsonld-to-turtle [json]
+(def rdf-language { :ntriples Lang/NTRIPLES
+                    :turtle Lang/TURTLE
+                    :rdfxml Lang/RDFXML })
+
+(defn jsonld-to-rdf [json lang]
   (let [jsonstr  (json/generate-string json)
         stream (ByteArrayInputStream. (.getBytes jsonstr "UTF-8"))
         writer (StringWriter. )
         base  "app://6b16aa40-ae2a-4fbc-9c8d-321464f03f3d/"
         model (ModelFactory/createDefaultModel)]
     (RDFDataMgr/read model stream base JenaJSONLD/JSONLD)
-    (RDFDataMgr/write writer model Lang/TURTLE)
+    (RDFDataMgr/write writer model (rdf-language lang))
     (str writer)))
+
 
 
--- test/scufl2_info/test/jsonld.clj
+++ test/scufl2_info/test/jsonld.clj
@@ -2,17 +2,20 @@
   (:use clojure.test
         scufl2-info.jsonld))
 
+(defn verify-turtle [turtle]
+      (is (. turtle contains "<http://example.com/a>"))
+      (is (. turtle contains " a "))
+      (is (. turtle contains "<http://example.org/b>"))
+      (is (. turtle contains "<http://example.net/c>"))
+      (is (. turtle contains "\"d\"")))
+
 (deftest test-jsonld
   (testing "jsonld-to-turtle"
     (let [jsonld { "@id"   "http://example.com/a"
                    "@type" "http://example.org/b" 
                    "http://example.net/c" "d" }
-          turtle (jsonld-to-turtle jsonld)]
-      (is (. turtle contains "<http://example.com/a>"))
-      (is (. turtle contains " a "))
-      (is (. turtle contains "<http://example.org/b>"))
-      (is (. turtle contains "<http://example.net/c>"))
-      (is (. turtle contains "\"d\"")))))
+          turtle (jsonld-to-rdf jsonld :turtle)]
+      (verify-turtle turtle))))
 
 
               
