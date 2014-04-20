12e2e75346f4fd9787461640d4cc04933a073549
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2014-04-20 23:00:22 +0100

    dataset

--- src/scufl2_info/jsonld.clj
+++ src/scufl2_info/jsonld.clj
@@ -5,6 +5,7 @@
   (:import org.apache.jena.riot.RDFDataMgr)
   (:import org.apache.jena.riot.Lang)
   (:import com.hp.hpl.jena.rdf.model.ModelFactory)
+  (:import com.hp.hpl.jena.query.DatasetFactory)
   (:import java.io.StringReader)
   (:import java.io.StringWriter)
   (:import java.io.ByteArrayInputStream)
@@ -24,6 +25,7 @@
         stream (ByteArrayInputStream. (.getBytes jsonstr "UTF-8"))
         writer (StringWriter. )
         base  "app://6b16aa40-ae2a-4fbc-9c8d-321464f03f3d/"
+        ;dataset (DatasetFactory/createMem)
         model (ModelFactory/createDefaultModel)]
     (RDFDataMgr/read model stream base JenaJSONLD/JSONLD)
     (RDFDataMgr/write writer model (rdf-language lang))
