af1bc0294a1659342612a241f239f8eba79887a1
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2014-04-21 00:09:33 +0100

    test jsonld later

--- test/scufl2_info/test/jsonld.clj
+++ test/scufl2_info/test/jsonld.clj
@@ -3,6 +3,20 @@
   (:use clojure.test
         scufl2-info.jsonld))
 
+(deftest test-content-types-of-lang
+    (is (= "application/ld+json" (first (content-types-of-lang JenaJSONLD/JSONLD)))))
+
+(deftest test-rdf-content-types
+  (testing "contains turtle"
+    (is (contains? rdf-content-types "text/turtle"))
+    (is (contains? rdf-content-types "application/turtle")))
+  (testing "contains trig"
+    (is (contains? rdf-content-types "application/trig")))
+    (is (contains? rdf-content-types "text/trig"))
+  (testing "contains jsonld"
+    (is (contains? rdf-content-types "application/ld+json"))))
+
+
 (defn verify-turtle [turtle]
   ;(print turtle)
       (is (. turtle contains "<http://example.com/a>"))
@@ -23,6 +37,7 @@
               "@type" "http://example.org/b" 
               "http://example.net/c" "d" })
 
+
 (deftest test-jsonld
   (testing "jsonld-to-turtle"
     (let [ turtle (jsonld-to-rdf jsonld :turtle)
@@ -31,20 +46,4 @@
       (verify-turtle turtle)
       (verify-turtle ntriples)
       (verify-rdfxml rdfxml))))
-
-(deftest test-content-types-of-lang
-    (is (= "application/ld+json" (first (content-types-of-lang JenaJSONLD/JSONLD)))))
-
-(deftest test-rdf-content-types
-  (testing "contains turtle"
-    (is (contains? rdf-content-types "text/turtle"))
-    (is (contains? rdf-content-types "application/turtle")))
-  (testing "contains trig"
-    (is (contains? rdf-content-types "application/trig")))
-    (is (contains? rdf-content-types "text/trig"))
-  (testing "contains jsonld"
-    (is (contains? rdf-content-types "application/ld+json"))))
-
-              
-
 
