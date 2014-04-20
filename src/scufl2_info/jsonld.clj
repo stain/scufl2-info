2a2dd8081355aee1c8a8307b27b3ab0605b1ffb1
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2014-04-20 23:17:16 +0100

    generic nameToLang

--- src/scufl2_info/jsonld.clj
+++ src/scufl2_info/jsonld.clj
@@ -17,17 +17,13 @@
 ; Initialize Jena binding once -- is this needed as long as we do import?
 (JenaJSONLD/init)
 
-(def rdf-language { :ntriples Lang/NTRIPLES
-                    :turtle Lang/TURTLE
-                    :rdfxml Lang/RDFXML })
-
 (defn jsonld-to-rdf [json lang]
   (let [jsonstr  (json/generate-string json)
         stream (ByteArrayInputStream. (.getBytes jsonstr "UTF-8"))
         writer (StringWriter. )
         base  "app://6b16aa40-ae2a-4fbc-9c8d-321464f03f3d/"
         dataset (DatasetFactory/createMem)
-        lang (rdf-language lang)
+        lang (RDFLanguages/nameToLang (name lang))
         ;model (ModelFactory/createDefaultModel)
     ]
     (RDFDataMgr/read dataset stream base JenaJSONLD/JSONLD)
