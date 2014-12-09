59b9ac7e2371f2f60bce29d5e54ac7aa081790b6
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2014-12-09 15:06:40 +0000

    Missing dependencies added

--- project.clj
+++ project.clj
@@ -5,6 +5,8 @@
                  [org.clojure/clojure "1.5.1"]
                  [com.github.jsonld-java/jsonld-java-jena "0.3"]
                  [ring-middleware-format "0.3.2"]
+                 [ring/ring-json "0.3.1"]
+                 [com.gfredericks/catch-data "0.1.3"]
                  [compojure "1.1.6"]]
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler scufl2-info.handler/app}
