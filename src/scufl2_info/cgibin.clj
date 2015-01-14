3ec710ddeaae880a4ab07b1d0691aa85a023a246
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2015-01-14 22:37:08 +0000

    Handle all cgi-bin headers somewhat correctly

--- src/scufl2_info/cgibin.clj
+++ src/scufl2_info/cgibin.clj
@@ -1,31 +1,77 @@
 (ns scufl2-info.cgibin
   (:gen-class)
+  (:import [java.util Locale]
+           [java.net URI])
   (:require
     [ring.mock.request :as mock]
     [scufl2-info.handler :as handler]
     ))
 
+(defn convert-cgi-header [header]
+  (.. header
+    (replaceFirst "HTTP_" "")
+    (replace "_" "-")
+    (toLowerCase Locale/US)
+  ))
+
+(defn select-http-headers [header]
+  (.startsWith header "HTTP_"))
+
+(defn convert-headers []
+  (let [http-keys (filter select-http-headers (keys (System/getenv)))]
+    (zipmap
+      (map convert-cgi-header http-keys)
+      (vals (select-keys (System/getenv) http-keys)))))
+
+(defn print-header [[k,v]]
+  (println (str k ": " v)))
 
 (defn -main [& args]
 
-  (let [path (or (System/getenv "PATH_INFO") "/")
-        method (or (System/getenv "REQUEST_METHOD") "GET")
-        request (mock/request (keyword (.toLowerCase method)) path)
-        ; TODO - all the HTTP_ environment variables
-        ; should be translated into HTTP headers, e.g.
-        ;HTTP_USER_AGENT=Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:34.0) Gecko/20100101 Firefox/34.0
-        ; into
-        ; :headers {"user-agent" "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:34.0) Gecko/20100101 Firefox/34.0" }
+  (let [
+        uri (or (System/getenv "PATH_INFO") "/")
+        request-method (or (System/getenv "REQUEST_METHOD") "GET")
+        auth-type (System/getenv "AUTH_TYPE")
+        content-length (System/getenv "CONTENT_LENGTH")
+        content-type (System/getenv "CONTENT_TYPE")
+        server-port (or (System/getenv "SERVER_PORT") "80")
+        server-name (or (System/getenv "SERVER_NAME") "localhost")
+        scheme (or (System/getenv "SERVER_PROTOCOL") "http")
+        remote-addr (or (System/getenv "REMOTE_ADDR") "127.0.0.1")
+        remote-host (or (System/getenv "REMOTE_HOST") "localhost")
+        remote-ident (System/getenv "REMOTE_IDENT")
+        remote-user (System/getenv "REMOTE_USER")
+        query-string (System/getenv "QUERY_STRING")
+        ; see http://www.ietf.org/rfc/rfc3875
+        request { :request-method (keyword (.toLowerCase request-method))
+                  :uri (.getRawPath (URI. uri))
+                  :server-port (Integer/parseInt server-port)
+                  :server-name server-name
+                  :scheme scheme
+                  :remote-addr remote-addr
+                  :remote-host remote-host
+                  :remote-ident remote-ident
+                  :remote-user remote-user
+                  :query-string query-string
+                  :content-length (and content-length (Long/parseLong content-length))
+                  :content-type content-type
+                  :auth-type auth-type
+                  :headers (convert-headers) }
+        ; TODO read in content-length submitted bytes and store in :body
         response (handler/app request)]
 
-
-
-  (println "Content-Type: text/plain")
-  (println "")
-
-  (println "This was made by Clojure")
-  (println path)
-  (println args)
-  (println request)
-  (println response)
-  ))
+  ; FIXME: This is a potential security breach
+  (if (and query-string (.contains query-string "_cgi=debug"))
+    (do
+      (println "Content-Type: text/plain")
+      (println "")
+      (println "Debug from cgibin.clj")
+      (println)
+      (println "env") (doall (map println (System/getenv)))
+      (println "request") (println request))
+    (do
+      (println "Status:" (:status response))
+      (doall (map print-header (:headers response)))
+      (println "")
+      ; Should probably do (print do handle binaries - but we know it's all HTML and JSON)
+      (println (:body response))))))
